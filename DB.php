a4d54bdd43be5b09d89d9935d33ef578c529ad67
Pavel <pavel.ps2014@yandex.ru>
2022-03-10 13:56:17 +0300

    Отформатировал

--- DB.php
+++ DB.php
@@ -1,50 +1,39 @@
-<?php 
+<?php
 
-	//use PDO;
 
-	class DB
+
+class DB
+{
+	private $db;
+	public function __construct()
 	{
-		// Объект класса PDO
-		private $db;
-
-		// Соединение с БД
-		public function __construct()
-		{
-			$dbinfo = require './dbinfo.php';
-			$this->db = new PDO('mysql:host=' . $dbinfo['host'] . ';dbname=' . $dbinfo['dbname'], $dbinfo['login'], $dbinfo['password']);
-		}
-
-		// Операции над БД
-		public function query($sql, $params = [])
-		{
-			// Подготовка запроса
-			$stmt = $this->db->prepare($sql);
-			
-			// Обход массива с параметрами 
-			// и подставляем значения
-			if ( !empty($params) ) {
-				foreach ($params as $key => $value) {
-					$stmt->bindValue(":$key", $value);
-				}
-			}
-			
-			// Выполняя запрос
-			$stmt->execute();
-			// Возвращаем ответ
-			return $stmt->fetchAll(PDO::FETCH_ASSOC);
-		}
-
-		public function getAll($table, $sql = '', $params = [])
-		{
-			return $this->query("SELECT * FROM $table" . $sql, $params);
-		}
-
-		public function getRow($table, $sql = '', $params = [])
-		{
-			$result = $this->query("SELECT * FROM $table" . $sql, $params);
-			return $result[0]; 
-		}
-
+		$dbinfo = require './dbinfo.php';
+		$this->db = new PDO('mysql:host=' . $dbinfo['host'] . ';dbname=' . $dbinfo['dbname'], $dbinfo['login'], $dbinfo['password']);
 	}
 
-?>
+
+	public function query($sql, $params = [])
+	{
+		$stmt = $this->db->prepare($sql);
+		if (!empty($params)) {
+			foreach ($params as $key => $value) {
+				$stmt->bindValue(":$key", $value);
+			}
+		}
+
+		$stmt->execute();
+		return $stmt->fetchAll(PDO::FETCH_ASSOC);
+	}
+
+	public function getAll($table, $sql = '', $params = [])
+	{
+		return $this->query("SELECT * FROM $table" . $sql, $params);
+	}
+
+
+	public function getRow($table, $sql = '', $params = [])
+	{
+		$result = $this->query("SELECT * FROM $table" . $sql, $params);
+		return $result[0];
+	}
+}
--- index.php
+++ index.php
@@ -1,26 +1,23 @@
 <?php
-	include('./DB.php');
-	$db = new DB;
+require('./DB.php');
+$db = new DB;
+$res = $db->getAll('oc_product');
 
-	
-	echo "<h1>До</h1><pre>";
-	$res= $db->getAll('oc_product');
 
-	
-		
-	$xml = new DomDocument('1.0','utf-8');
-	$products = $xml->appendChild($xml->createElement('products'));
-	foreach ($res as $data) {
+
+$xml = new DomDocument('1.0', 'utf-8');
+$products = $xml->appendChild($xml->createElement('products'));
+foreach ($res as $data) {
 	$product = $products->appendChild($xml->createElement('product'));
-	$product_id=$product->appendChild($xml->createElement('product_id'));
+	$product_id = $product->appendChild($xml->createElement('product_id'));
 	$model = $product->appendChild($xml->createElement('name'));
-    $sku = $product->appendChild($xml->createElement('scu'));
-	$upc= $product->appendChild($xml->createElement('upc'));
+	$sku = $product->appendChild($xml->createElement('scu'));
+	$upc = $product->appendChild($xml->createElement('upc'));
 	$ean = $product->appendChild($xml->createElement('ean'));
 	$jan = $product->appendChild($xml->createElement('jan'));
 	$isbn = $product->appendChild($xml->createElement('isbn'));
 	$mpn = $product->appendChild($xml->createElement('mpn'));
-	$location = $product->appendChild($xml->createElement(''));
+	$location = $product->appendChild($xml->createElement('location'));
 	$quantity = $product->appendChild($xml->createElement('quantity'));
 	$stock_status_id = $product->appendChild($xml->createElement('stock_status_id'));
 	$image = $product->appendChild($xml->createElement('image'));
@@ -44,38 +41,38 @@
 	$date_added = $product->appendChild($xml->createElement('date_added'));
 	$date_modified = $product->appendChild($xml->createElement('date_modified'));
 
-    $product_id->appendChild($xml->createTextNode($data['product_id']));
+	$product_id->appendChild($xml->createTextNode($data['product_id']));
 	$model->appendChild($xml->createTextNode($data['model']));
 	$sku->appendChild($xml->createTextNode($data['sku']));
-    $upc->appendChild($xml->createTextNode($data['upc']));
+	$upc->appendChild($xml->createTextNode($data['upc']));
 	$ean->appendChild($xml->createTextNode($data['ean']));
-	$jan ->appendChild($xml->createTextNode($data['jan']));
-	$isbn ->appendChild($xml->createTextNode($data['isbn']));
-	$mpn ->appendChild($xml->createTextNode($data['mpn']));
-	$location ->appendChild($xml->createTextNode($data['location']));
-	$quantity ->appendChild($xml->createTextNode($data['quantity']));
-	$stock_status_id ->appendChild($xml->createTextNode($data['stock_status_id']));
-	$image ->appendChild($xml->createTextNode($data['image']));
-	$manufacturer_id ->appendChild($xml->createTextNode($data['manufacturer_id']));
-	$shipping ->appendChild($xml->createTextNode($data['shipping']));
-	$price ->appendChild($xml->createTextNode($data['price']));
-	$points ->appendChild($xml->createTextNode($data['points']));
-	$tax_class_id ->appendChild($xml->createTextNode($data['tax_class_id']));
-	$date_available ->appendChild($xml->createTextNode($data['date_available']));
+	$jan->appendChild($xml->createTextNode($data['jan']));
+	$isbn->appendChild($xml->createTextNode($data['isbn']));
+	$mpn->appendChild($xml->createTextNode($data['mpn']));
+	$location->appendChild($xml->createTextNode($data['location']));
+	$quantity->appendChild($xml->createTextNode($data['quantity']));
+	$stock_status_id->appendChild($xml->createTextNode($data['stock_status_id']));
+	$image->appendChild($xml->createTextNode($data['image']));
+	$manufacturer_id->appendChild($xml->createTextNode($data['manufacturer_id']));
+	$shipping->appendChild($xml->createTextNode($data['shipping']));
+	$price->appendChild($xml->createTextNode($data['price']));
+	$points->appendChild($xml->createTextNode($data['points']));
+	$tax_class_id->appendChild($xml->createTextNode($data['tax_class_id']));
+	$date_available->appendChild($xml->createTextNode($data['date_available']));
 	$weight->appendChild($xml->createTextNode($data['weight']));
-	$weight_class_id ->appendChild($xml->createTextNode($data['weight_class_id']));
-	$length ->appendChild($xml->createTextNode($data['length']));
-	$width ->appendChild($xml->createTextNode($data['width']));
-	$height ->appendChild($xml->createTextNode($data['height']));
-	$length_class_id ->appendChild($xml->createTextNode($data['length_class_id']));
-	$subtract ->appendChild($xml->createTextNode($data['subtract']));
-	$minimum ->appendChild($xml->createTextNode($data['minimum']));
-	$sort_order ->appendChild($xml->createTextNode($data['sort_order']));
-	$status ->appendChild($xml->createTextNode($data['status']));
-	$viewed ->appendChild($xml->createTextNode($data['viewed']));
-	$date_added ->appendChild($xml->createTextNode($data['date_added']));
-	$date_modified ->appendChild($xml->createTextNode($data['date_modified']));
+	$weight_class_id->appendChild($xml->createTextNode($data['weight_class_id']));
+	$length->appendChild($xml->createTextNode($data['length']));
+	$width->appendChild($xml->createTextNode($data['width']));
+	$height->appendChild($xml->createTextNode($data['height']));
+	$length_class_id->appendChild($xml->createTextNode($data['length_class_id']));
+	$subtract->appendChild($xml->createTextNode($data['subtract']));
+	$minimum->appendChild($xml->createTextNode($data['minimum']));
+	$sort_order->appendChild($xml->createTextNode($data['sort_order']));
+	$status->appendChild($xml->createTextNode($data['status']));
+	$viewed->appendChild($xml->createTextNode($data['viewed']));
+	$date_added->appendChild($xml->createTextNode($data['date_added']));
+	$date_modified->appendChild($xml->createTextNode($data['date_modified']));
 }
-	$xml->formatOutput = true;
-	
-$xml->save('myxml.xml');
+$xml->formatOutput = true;
+
+$xml->save('products.xml');
